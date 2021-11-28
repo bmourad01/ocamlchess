d6eaed98ab107a086e5e5cef5627d77833971ee5
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2021-11-28 15:31:19 -0500

    Adds an interactive testing envrionment with SFML

---
+++ bin/test_gui.ml
@@ -0,0 +1,93 @@
+open Core_kernel
+open Chess
+
+module Window = struct
+  type t
+
+  type event = [
+    | `MouseButtonPressed of int * int
+  ]
+
+  external create : int -> int -> string -> t = "ml_window_create"
+  external size : t -> (int * int) = "ml_window_size"
+  external is_open : t -> bool = "ml_window_is_open"
+  external poll_event : t -> event option = "ml_window_poll_event"
+  external clear : t -> unit = "ml_window_clear"
+  external display : t -> unit = "ml_window_display"
+
+  external paint_board :
+    t -> Position.t -> int64 -> Square.t option -> Move.t option -> unit =
+    "ml_window_paint_board"
+end
+
+let screen_to_sq window mx my =
+  let sx, sy = Window.size window in
+  let tw, th = sx / Square.File.count, sy / Square.Rank.count in
+  let rec loop_y y =
+    if y >= Square.Rank.count then None
+    else
+      let rec loop_x x =
+        if x >= Square.File.count then None
+        else
+          let px = x * tw in
+          let py = y * th in
+          if mx >= px && mx < (px + tw) && my >= py && my < (py + th)
+          then Square.create ~rank:(Square.Rank.count - 1 - y) ~file:x
+          else succ x |> loop_x in
+      match loop_x 0 with
+      | None -> succ y |> loop_y
+      | Some _ as sq -> sq in
+  loop_y 0
+
+let poll window pos legal sel prev =
+  match Window.poll_event window with
+  | None -> pos, legal, sel, prev
+  | Some (`MouseButtonPressed (mx, my)) ->
+    match screen_to_sq window mx my with
+    | None -> pos, legal, sel, prev
+    | Some sq -> match sel with
+      | None ->
+        let moves =
+          List.filter legal ~f:(fun (m, _) -> Square.(sq = Move.src m)) in
+        let sel = if List.is_empty moves then None else Some (sq, moves) in
+        pos, legal, sel, prev
+      | Some (sq', _) when Square.(sq = sq') ->
+        pos, legal, None, prev
+      | Some (_, moves) ->
+        let move =
+          List.find moves ~f:(fun (m, _) -> Square.(sq = Move.dst m)) in
+        match move with
+        | None -> pos, legal, sel, prev
+        | Some (m, pos) ->
+          let legal = Position.legal_moves pos in
+          pos, legal, None, Some m
+
+let rec main_loop window pos legal sel prev =
+  if Window.is_open window then begin
+    let pos, legal, sel, prev = poll window pos legal sel prev in
+    let bb, sq = match sel with
+      | None -> Bitboard.(to_int64 empty), None
+      | Some (sq, moves) ->
+        let bb =
+          List.fold moves ~init:Bitboard.empty ~f:(fun acc (m, _) ->
+              Bitboard.(acc ++ Move.dst m)) in
+        Bitboard.to_int64 bb, Some sq in
+    Window.clear window;
+    Window.paint_board window pos bb sq prev;
+    Window.display window;
+    main_loop window pos legal sel prev
+  end
+
+let () = Callback.register "piece_at_square" Position.piece_at_square
+let () = Callback.register "string_of_square" Square.to_string
+
+let window_size = 640
+
+external init_fonts : unit -> bool = "ml_init_fonts"
+
+let () =
+  if init_fonts () then
+    let window = Window.create window_size window_size "chess" in
+    let pos = Position.start in
+    let legal = Position.legal_moves pos in
+    main_loop window pos legal None None
---
+++ bin/test_gui_stubs.cpp
@@ -0,0 +1,222 @@
+#include <cassert>
+#include <cstdio>
+
+#include <caml/alloc.h>
+#include <caml/callback.h>
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/memory.h>
+#include <caml/mlvalues.h>
+#include <caml/version.h>
+
+#include <SFML/Graphics.hpp>
+
+static const char *const _piece_font_filename =
+    "/usr/share/fonts/TTF/FreeSerif.ttf";
+static const char *const _text_font_filename =
+    "/usr/share/fonts/TTF/FreeSans.ttf";
+
+static sf::Font _piece_font;
+static sf::Font _text_font;
+
+#define Sfml_window_val(v)                                                     \
+  (*reinterpret_cast<sf::RenderWindow **>(Data_custom_val(window)))
+
+#define Make_square(x, y) ((unsigned)(((y) << 3) | (x)))
+#define Move_src(m) (Int_val(m) & 0b111111)
+#define Move_dst(m) ((Int_val(m) >> 6) & 0b111111)
+#define Piece_color(p) ((Int_val(p) >> 3) & 0b1)
+#define Piece_kind(p) (Int_val(p) & 0b111)
+
+extern "C" {
+  value ml_init_fonts(value dummy) {
+    CAMLparam1(dummy);
+    CAMLlocal1(result);
+    result = Val_bool(true);
+    if (!_piece_font.loadFromFile(_piece_font_filename)) {
+      fprintf(stderr, "couldn't load font %s\n", _piece_font_filename);
+      result = Val_bool(false);
+    }
+    if (!_text_font.loadFromFile(_text_font_filename)) {
+      fprintf(stderr, "couldn't load font %s\n", _text_font_filename);
+      result = Val_bool(false);
+    }
+    CAMLreturn(result);
+  }
+
+  static void sfml_finalize_window(value window) {
+    CAMLparam1(window);
+    auto sf_window = Sfml_window_val(window);
+    delete sf_window;
+    CAMLreturn0;
+  }
+  
+  static struct custom_operations sfml_window_custom_ops = {
+    (char*)"sfml_window_custom_ops",
+    sfml_finalize_window,
+    custom_compare_default,
+    custom_compare_ext_default,
+    custom_hash_default,
+    custom_serialize_default,
+    custom_deserialize_default,
+#if OCAML_VERSION_MAJOR >= 4 && OCAML_VERSION_MINOR >= 8
+    NULL, // custom_fixed_length
+#endif
+  };
+
+  value ml_window_create(value w, value h, value name) {
+    CAMLparam3(w, h, name);
+    CAMLlocal1(window);
+    int width = Int_val(w);
+    int height = Int_val(h);
+    const char *name_ptr = String_val(name);
+    auto sf_window = new sf::RenderWindow(sf::VideoMode(width, height), name_ptr);
+    window = caml_alloc_custom(&sfml_window_custom_ops,
+                               sizeof(sf::RenderWindow *), 0, 1);
+    Sfml_window_val(window) = sf_window;
+    CAMLreturn(window);
+  }
+
+  value ml_window_size(value window) {
+    CAMLparam1(window);
+    CAMLlocal1(tmp);
+    auto size = Sfml_window_val(window)->getSize();
+    tmp = caml_alloc(2, 0);
+    Store_field(tmp, 0, Val_int(size.x));
+    Store_field(tmp, 1, Val_int(size.y));
+    CAMLreturn(tmp);
+  }
+
+  value ml_window_is_open(value window) {
+    CAMLparam1(window);
+    CAMLreturn(Val_bool(Sfml_window_val(window)->isOpen()));
+  }
+
+  value ml_window_poll_event(value window) {
+    CAMLparam1(window);
+    CAMLlocal2(result, mouse_coords);
+    auto sf_window = Sfml_window_val(window);
+    sf::Event event;
+    result = Val_none;
+    while (sf_window->pollEvent(event)) {
+      switch (event.type) {
+      case sf::Event::Closed:
+        sf_window->close();
+        break;
+      case sf::Event::MouseButtonPressed:
+        result = caml_alloc(2, 0);
+        Store_field(result, 0, Val_int(hash_variant("MouseButtonPressed")));
+        mouse_coords = caml_alloc(2, 0);
+        Store_field(mouse_coords, 0, Val_int(event.mouseButton.x));
+        Store_field(mouse_coords, 1, Val_int(event.mouseButton.y));
+        Store_field(result, 1, mouse_coords);
+        result = caml_alloc_some(result);
+        break;
+      default:
+        break;
+      }
+    }
+    CAMLreturn(result);
+  }
+
+  value ml_window_clear(value window) {
+    CAMLparam1(window);
+    Sfml_window_val(window)->clear();
+    CAMLreturn(Val_unit);
+  }
+
+  value ml_window_display(value window) {
+    CAMLparam1(window);
+    Sfml_window_val(window)->display();
+    CAMLreturn(Val_unit);
+  }
+
+  static wchar_t piece_unicode(int color, int kind) {
+    auto is_white = color != 0b1;
+    switch (kind) {
+    case 0b000:
+      return is_white ? L'\u2659' : L'\u265F';
+    case 0b011:
+      return is_white ? L'\u2656' : L'\u265C';
+    case 0b010:
+      return is_white ? L'\u2657' : L'\u265D';
+    case 0b001:
+      return is_white ? L'\u2658' : L'\u265E';
+    case 0b101:
+      return is_white ? L'\u2654' : L'\u265A';
+    case 0b100:
+      return is_white ? L'\u2655' : L'\u265B';
+    default:
+      assert(false);
+    }
+  }
+
+  value ml_window_paint_board(value window,
+                              value position,
+                              value bitboard,
+                              value square,
+                              value prev) {
+    CAMLparam5(window, position, bitboard, square, prev);
+    CAMLlocal2(piece, sq_string);
+    auto sf_window = Sfml_window_val(window);
+    auto size = sf_window->getSize();
+    int tw = size.x / 8;
+    int th = size.y / 8;
+    int sz = (tw + th) / 2;
+    int sz2 = (tw + th) / 12;
+    auto bb = Int64_val(bitboard);
+    for (int y = 0; y < 8; ++y) {
+      for (int x = 0; x < 8; ++x) {
+        auto sq = Make_square(x, 7 - y);
+        sf::RectangleShape tile(sf::Vector2f(tw, th));
+        if (square != Val_none && Int_val(Some_val(square)) == sq) {
+          tile.setFillColor(sf::Color(0xF3, 0xF7, 0x81));
+        } else if (bb & (1ULL << sq)) {
+          tile.setFillColor(sf::Color(0x2E, 0xCC, 0xFA));
+        } else if (prev != Val_none &&
+                   (Move_src(Some_val(prev)) == sq ||
+                    Move_dst(Some_val(prev)) == sq)) {
+          tile.setFillColor(sf::Color(0xCC, 0xCC, 0xCC));
+        } else {
+          if (y % 2) {
+            if (x % 2) {
+              tile.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+          } else {
+              tile.setFillColor(sf::Color(0xF7, 0xF2, 0xF0));
+            }
+          } else {
+            if (x % 2) {
+              tile.setFillColor(sf::Color(0xF7, 0xF2, 0xF0));
+            } else {
+              tile.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+            }
+          }
+        }
+        tile.setPosition(x * tw, y * th);
+        sf_window->draw(tile);
+        piece = caml_callback2(*caml_named_value("piece_at_square"),
+                               position,
+                               Val_int(sq));
+        if (piece != Val_none) {
+          auto unicode = piece_unicode(Piece_color(Some_val(piece)),
+                                       Piece_kind(Some_val(piece)));
+          sf::Text piece_text(unicode, _piece_font, sz);
+          float fx = x * tw;
+          float fy = y * th;
+          float xa = ((float)(tw - sz) / 2.0f) + ((float)sz / 8.5f);
+          float ya = ((float)(th - sz) / 10.0f) - ((float)sz / 5.0f);
+          piece_text.setPosition(fx + xa, fy + ya);
+          piece_text.setFillColor(sf::Color::Black);
+          sf_window->draw(piece_text);
+        }
+        sq_string = caml_callback(*caml_named_value("string_of_square"),
+                                  Val_int(sq));
+        sf::Text tile_text(String_val(sq_string), _text_font, sz2);
+        tile_text.setPosition(x * tw, y * th);
+        tile_text.setFillColor(sf::Color(0x00, 0x00, 0x00, 0x5F));
+        sf_window->draw(tile_text);
+      }
+    }
+    CAMLreturn(Val_unit);
+  }
+}
