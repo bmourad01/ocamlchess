b77b642008532d35e86f2cd7da7c33d68541d22d
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2021-12-20 07:55:17 -0500

    Simplify

--- src/player/player_same_color.ml
+++ src/player/player_same_color.ml
@@ -14,19 +14,15 @@ class cls ?(limits = None) () = object(self)
     | White -> (rank land 1) <> (file land 1)
     | Black -> (rank land 1) = (file land 1)
 
-  method private board active = match (active : Piece.color) with
-    | White -> Position.white
-    | Black -> Position.black
-  
   method move pos = match Position.legal_moves pos with
     | [] -> raise Player.No_moves
     | moves ->
       let active = Position.active pos in
-      let board = self#board active in
       self#equal_eval moves ~eval:(fun mv ->
           let pos = Position.Legal_move.position mv in
           Option.return @@ Bb.count @@
-          Bb.filter (board pos) ~f:(self#same_color active)) |>
+          Bb.filter ~f:(self#same_color active) @@
+          Position.board_of_color pos active) |>
       List.random_element_exn
   
   method name = "same-color"
