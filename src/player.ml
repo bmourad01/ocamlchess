698d7dcebdb36d52dd5a6dc9738044c983c97630
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2021-12-25 13:04:51 -0500

    Neater

--- src/player.ml
+++ src/player.ml
@@ -21,11 +21,9 @@ let create ~choose = fun ?(limits = None) () -> {choose; limits}
 let best_moves moves ~eval =
   let open Option.Monad_infix in
   List.filter_map moves ~f:(fun m -> eval m >>| fun score -> (m, score)) |>
-  List.sort ~compare:(fun (_, score) (_, score') ->
-      Int.compare score' score) |>
+  List.sort ~compare:(fun (_, a) (_, b) -> Int.compare b a) |>
   List.fold_until ~init:([], 0) ~finish:fst
-    ~f:(fun (acc, score') (m, score) ->
-        match acc with
+    ~f:(fun (acc, score') (m, score) -> match acc with
         | [] -> Continue (m :: acc, score)
         | _ when score' > score -> Stop acc
         | _ -> Continue (m :: acc, score'))
