c8354a15bc1383b320b3172e332b759b913b5b1f
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2021-12-27 12:29:20 -0500

    Add time and nodes per second statistics to `perft` command

--- chess/bin/perft.ml
+++ chess/bin/perft.ml
@@ -1,11 +1,12 @@
 open Core_kernel
 open Chess
 
+module Legals = Position.Legal_moves
+
 let rec perft ?(first = false) pos depth =
   if depth <= 0 then 1L
   else
-    Position.legal_moves pos |>
-    Position.Legal_moves.moves |>
+    Position.legal_moves pos |> Legals.moves |>
     List.fold ~init:0L ~f:(fun acc mv ->
         let m, pos = Position.Legal_move.decomp mv in
         let nodes = perft pos (depth - 1) in
@@ -15,5 +16,13 @@ let rec perft ?(first = false) pos depth =
         Int64.(acc + nodes))
 
 let go depth pos =
+  let t = Time.now () in
   let n = perft pos depth ~first:true in
-  printf "\nNodes searched: %Ld\n%!" n
+  let t' = Time.now () in
+  let sec = Time.(Span.to_sec @@ diff t' t) in
+  let nps = Float.to_int64 (Float.of_int64 n /. sec) in
+  let istr = Int64.to_string_hum ~delimiter:',' in
+  printf "\n%!";
+  printf "Time taken: %fs\n%!" sec;
+  printf "Nodes searched: %s\n%!" @@ istr n;
+  printf "Nodes per second: %s\n%!" @@ istr nps
