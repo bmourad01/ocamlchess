884d656bfff7ad85e87a88918a969ca9ccf9073b
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2021-12-29 17:30:14 -0500

    Fixes the `pacifist` player

--- chess/src/player/player_pacifist.ml
+++ chess/src/player/player_pacifist.ml
@@ -3,18 +3,6 @@ open Core_kernel
 module Bb = Bitboard
 module Legal = Position.Legal
 
-let avoid_checkmate = List.filter ~f:(fun m ->
-    let pos = Legal.new_position m in
-    match Position.legal_moves pos with
-    | [] -> not @@ Position.in_check pos
-    | _ -> true)
-
-let avoid_check = List.filter ~f:(fun m ->
-    Legal.new_position m |> Position.in_check |> not)
-
-let avoid_capture =
-  List.filter ~f:(fun m -> Option.is_none @@ Legal.capture m)
-
 let piece_value (k : Piece.kind) = match k with
   | Pawn -> 1
   | Knight | Bishop -> 3
@@ -22,21 +10,25 @@ let piece_value (k : Piece.kind) = match k with
   | Queen -> 9
   | King -> 0
 
-let capture_lowest_value =
-  Player.best_moves ~eval:(fun m ->
-      Legal.capture m |> Option.map ~f:(fun (k, _) -> -(piece_value k)))
-
 let choose _ = function
   | []-> raise Player.No_moves
   | moves ->
-    let moves = match avoid_checkmate moves with
-      | (_ :: _) as moves -> moves
-      | [] -> match avoid_check moves with
-        | (_ :: _) as moves -> moves
-        | [] -> match avoid_capture moves with
-          | (_ :: _) as moves -> moves
-          | [] -> capture_lowest_value moves in
-    List.random_element_exn moves
+    Player.best_moves moves ~eval:(fun m ->
+        let pos = Legal.new_position m in
+        let in_check = Position.in_check pos in
+        (* Give the highest possible penalty for checkmating the opponent.
+           Give a similarly high penalty for checking the opponent's king.
+           In all other cases, maximize the opponent's material advantage. 
+           This works to prioritize moves where we avoid capturing any piece. *)
+        let score = match Position.legal_moves pos with
+          | [] when in_check -> Int.min_value
+          | _ when in_check -> Int.min_value / 2
+          | _ ->
+            Position.find_active pos |>
+            List.fold ~init:0 ~f:(fun material (_, k) ->
+                material + piece_value k) in
+        Some score) |>
+    List.random_element_exn
 
 let create ?(limits = None) () = object
   method choose = choose
