b36603359e4be3b0fb03ebba8491775a9bf9c8ff
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-01-30 17:48:58 -0500

    Docs

--- chess/src/player.mli
+++ chess/src/player.mli
@@ -12,7 +12,8 @@ exception No_moves
 (** The player is expected to implement this interface.
 
     - [choose moves] will choose, from [moves], which move to play. May
-      raise [No_moves] if [moves] is empty.
+      raise [No_moves] if [moves] is empty. It is assumed that every
+      element of [moves] have the same parent position.
 
     - [limits] returns the search limits, if any, that were used to construct
       the player.
@@ -39,6 +40,6 @@ val register : t -> unit
     not exist, then [None] is returned. *)
 val lookup : string -> t option
 
-(** [enumerate ()] will return a list of all currently registered players
-    (in no particular order). *)
+(** [enumerate ()] will return a list of all currently registered players,
+    sorted by name. *)
 val enumerate : unit -> t list
