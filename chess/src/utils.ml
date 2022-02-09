21c9091f15035a58db1d72033731133cfdb03ae2
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-02-09 18:03:59 -0500

    Use mutable field of object

--- chess/src/utils.ml
+++ chess/src/utils.ml
@@ -6,13 +6,13 @@ module Prng = struct
   let create : int64 -> t = function
     | 0L -> invalid_arg "Seed must be nonzero"
     | seed -> object
-      val state = ref seed
+      val mutable state = seed
       method rand =
-        let s = !state in
+        let s = state in
         let s = Int64.(s lxor (s lsr 12)) in
         let s = Int64.(s lxor (s lsl 25)) in
         let s = Int64.(s lxor (s lsr 27)) in
-        state := s;
+        state <- s;
         Int64.(s * 2685821657736338717L)
     end
 end
