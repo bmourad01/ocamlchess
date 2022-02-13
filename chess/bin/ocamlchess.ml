e00d56931df074deb187c9dab430109da65da51b
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-02-12 21:07:06 -0500

    Edit desc

--- chess/bin/ocamlchess.ml
+++ chess/bin/ocamlchess.ml
@@ -21,7 +21,7 @@ module Default_player = struct
     m, transpositions
 
   let player = Player.create ~choice ~state:Int64.Map.empty ~name:"caml"
-      ~desc:"The default player, based on traditional game tree search and \
+      ~desc:"The flagship player, based on traditional game tree search and \
              evaluation"
 end
 
