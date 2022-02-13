ed24cd85b49147f8b9d83730650f31d69f9aa6a4
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-02-12 20:19:18 -0500

    Rename

--- chess/src/eval.ml
+++ chess/src/eval.ml
@@ -5,11 +5,11 @@ module Bb = Bitboard
 let calculate_material =
   let kinds = Piece.[Pawn; Knight; Bishop; Rook; Queen] in
   fun pos ->
-    let active = Position.active_board pos in
-    let inactive = Position.inactive_board pos in
+    let us = Position.active_board pos in
+    let them = Position.inactive_board pos in
     List.fold kinds ~init:0 ~f:(fun acc k ->
         let b = Position.board_of_kind pos k in
         let v = Piece.Kind.value k in
-        acc + (Bb.(count (b & active)) - Bb.(count (b & inactive))) * v)
+        acc + (Bb.(count (b & us)) - Bb.(count (b & them))) * v)
 
 let go pos = calculate_material pos
