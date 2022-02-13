9fccfda39239979d5f1a4e8533ffdeb60524a96c
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-02-12 20:18:31 -0500

    Neater

--- chess/src/eval.ml
+++ chess/src/eval.ml
@@ -2,24 +2,14 @@ open Core_kernel
 
 module Bb = Bitboard
 
-let pawn_val = Piece.Kind.value Pawn
-let knight_val = Piece.Kind.value Knight
-let bishop_val = Piece.Kind.value Bishop
-let rook_val = Piece.Kind.value Rook
-let queen_val = Piece.Kind.value Queen
-
-let calculate_material pos =
-  let active = Position.active_board pos in
-  let inactive = Position.inactive_board pos in
-  let pawn = Position.pawn pos in
-  let knight = Position.knight pos in
-  let bishop = Position.bishop pos in
-  let rook = Position.rook pos in
-  let queen = Position.queen pos in
-  (Bb.(count (pawn & active)) - Bb.(count (pawn & inactive))) * pawn_val +
-  (Bb.(count (knight & active)) - Bb.(count (knight & inactive))) * knight_val +
-  (Bb.(count (bishop & active)) - Bb.(count (bishop & inactive))) * bishop_val +
-  (Bb.(count (rook & active)) - Bb.(count (rook & inactive))) * rook_val +
-  (Bb.(count (queen & active)) - Bb.(count (queen & inactive))) * queen_val
+let calculate_material =
+  let kinds = Piece.[Pawn; Knight; Bishop; Rook; Queen] in
+  fun pos ->
+    let active = Position.active_board pos in
+    let inactive = Position.inactive_board pos in
+    List.fold kinds ~init:0 ~f:(fun acc k ->
+        let b = Position.board_of_kind pos k in
+        let v = Piece.Kind.value k in
+        acc + (Bb.(count (b & active)) - Bb.(count (b & inactive))) * v)
 
 let go pos = calculate_material pos
