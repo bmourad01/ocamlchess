4daf6b4919d89c8438e638583989125aed884594
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-02-20 16:42:21 -0500

    Fix typo

--- chess/src/eval.mli
+++ chess/src/eval.mli
@@ -1,5 +1,5 @@
 (** Returns a score for a position. The score represents the relative
     advantage of the active player. Note that draws and checkmates
     are not explored here, but instead are considered when searching
-    positions (see the [Search]) module. *)
+    positions (see the [Search] module). *)
 val go : Position.t -> int
