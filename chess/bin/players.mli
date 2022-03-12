a7eeab9c33f396c48f9fc252c21ce4183a5859dc
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-03-12 10:15:41 -0500

    Get rid of `open` in the `Players` module

--- chess/bin/players.mli
+++ chess/bin/players.mli
@@ -1,14 +1,11 @@
-open Base
-open Chess
-
 (** [register name player] will register [player] in the database. If a player
     with the same name already exists, then [Invalid_argument] is raised. *)
-val register : string -> (unit -> Player.e) -> unit
+val register : string -> (unit -> Chess.Player.e) -> unit
 
 (** [lookup name] will look up the player with the name [name]. If it does
     not exist, then [None] is returned. *)
-val lookup : string -> Player.e option
+val lookup : string -> Chess.Player.e option
 
 (** [enumerate ()] will return a sequence of all currently registered players,
     sorted by name. *)
-val enumerate : unit -> Player.e Sequence.t
+val enumerate : unit -> Chess.Player.e Base.Sequence.t
