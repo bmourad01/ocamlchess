765be0eb3ebb9d45eda0821da33af91619b3c53c
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-04-23 20:09:55 -0400

    Fix out of bounds

--- chess/src/precalculated.ml
+++ chess/src/precalculated.ml
@@ -331,15 +331,16 @@ let[@inline] between sq1 sq2 =
 let mvv_lva =
   let victims = Piece.[Pawn; Knight; Bishop; Rook; Queen; King] in
   let attackers = List.rev victims in
-  let len = Piece.Kind.count in
+  let n = Piece.Kind.count in
+  let len = n * n in
   let tbl = Array.create ~len 0 in
   List.fold victims ~init:0 ~f:(fun init victim ->
       let i = Piece.Kind.to_int victim in
       List.fold attackers ~init ~f:(fun value attacker ->
           let j = Piece.Kind.to_int attacker in
-          tbl.(i + j * len) <- value;
+          tbl.(i + j * n) <- value;
           value + 1)) |> ignore;
   fun victim attacker ->
     let i = Piece.Kind.to_int victim in
     let j = Piece.Kind.to_int attacker in
-    Array.unsafe_get tbl (i + j * len)
+    Array.unsafe_get tbl (i + j * n)
