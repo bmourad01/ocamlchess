d5e372764e34bfc483e8292dc08aa5978e99e3bb
Benjamin Mourad <Benjamin_Mourad@student.uml.edu>
2022-04-28 19:27:17 -0400

    Small cleanup + comments in SEE

--- chess/src/see.ml
+++ chess/src/see.ml
@@ -5,7 +5,7 @@ module Legal = Position.Legal
 module Pre = Precalculated
 
 (* Get the set of squares that are attacking the destination square. *)
-let[@inline] attacks pos dst occupied =
+let[@inline] attackers pos dst occupied =
   let open Bb.Syntax in
   let white = Position.white pos in
   let black = Position.black pos in
@@ -28,7 +28,7 @@ let[@inline] attacks pos dst occupied =
 
 (* Get the set of squares from sliding pieces that are attacking the
    destination square. *)
-let[@inline] attacks_sliding pos dst occupied c =
+let[@inline] sliders pos dst occupied c =
   let open Bb.Syntax in
   let diag = Pre.bishop dst occupied in
   let orth = Pre.rook dst occupied in
@@ -53,26 +53,32 @@ type state = {
 (* Maximum number of pieces in a legal position. *)
 let swap_len = 32
 
-let[@inline] init legal src dst pos victim =
+let[@inline] init legal from dst pos victim =
   let depth = 1 in
+  let attacker = Piece.kind @@ Position.piece_at_square_exn pos from in
   let swap = Array.create ~len:swap_len 0 in
-  let from = src in
-  let attackers = attacks pos dst @@ Position.all_board pos in
   let target_val = Piece.Kind.value victim in
+  swap.(0) <- target_val;
+  (* Compute the initial set of attackers at the destination square. *)
+  let attackers = attackers pos dst @@ Position.all_board pos in
+  (* Compute the mask for squares that we will consider to be occupied.
+     As the evaluation continues, less squares will be available since
+     the pieces at these squares will be exchanged. *)
   let occupation =
     if Legal.is_en_passant legal then
       let sq = Option.value_exn (Position.en_passant_pawn pos) in
       Bb.(full -- sq)
     else Bb.full in
-  swap.(0) <- target_val;
+  (* Remove the initial attacker from the board. *)
   let attackers = Bb.(attackers -- from) in
   let occupation = Bb.(occupation -- from) in
+  (* Now that the initial attacker is removed, compute sliding attacks
+     that may have been previously blocked by him. *)
   let all = Position.all_board pos in
   let active = Position.active pos in
   let side = Piece.Color.opposite active in
-  let sliding = attacks_sliding pos dst Bb.(all & occupation) active in
-  let attackers = Bb.((attackers + sliding) & occupation) in
-  let attacker = Piece.kind @@ Position.piece_at_square_exn pos src in
+  let sliders = sliders pos dst Bb.(all & occupation) active in
+  let attackers = Bb.((attackers + sliders) & occupation) in
   {from; attackers; occupation; target_val; depth; side; attacker}, swap
 
 let[@inline] rec evaluate swap depth =
@@ -93,6 +99,7 @@ let[@inline] see legal victim =
   let pos = Legal.parent legal in
   let st, swap = init legal src dst pos victim in
   let all = Position.all_board pos in
+  (* The order from least to most valuable attacker. *)
   let lva_order = [
     Position.pawn   pos, Piece.Pawn;
     Position.knight pos, Piece.Knight;
@@ -101,6 +108,8 @@ let[@inline] see legal victim =
     Position.queen  pos, Piece.Queen;
     Position.king   pos, Piece.King;
   ] in
+  (* Get the least valuable piece that is currently able to attack the
+     square. *)
   let[@inline] lva () =
     let mask = Bb.(st.attackers & Position.board_of_color pos st.side) in
     if not @@ (List.exists [@specialised]) lva_order ~f:(fun (b, k) ->
@@ -113,6 +122,7 @@ let[@inline] see legal victim =
     then raise Stop in
   (* Main loop. *)
   let rec loop () =
+    (* Any attackers left? *)
     if Bb.(st.attackers <> empty) then begin
       (* Find the least valuable attacker, if they exist. *)
       lva ();
@@ -120,15 +130,16 @@ let[@inline] see legal victim =
       let s1 = swap.(st.depth - 1) in
       let s = st.target_val - s1 in
       swap.(st.depth) <- s;
-      (* The exchange is clearly losing, so abort. *)
+      (* The exchange is clearly losing, so abort. This is a simple pruning
+         mechanism as it does not change the final evaluation. *)
       if max (-s1) s < 0 then raise Stop;
       st.depth <- st.depth + 1;
       st.target_val <- Piece.Kind.value st.attacker;
-      (* Update the board masks. *)
+      (* Update the board. *)
       st.attackers <- Bb.(st.attackers -- st.from);
       st.occupation <- Bb.(st.occupation -- st.from);
-      let sliding = attacks_sliding pos dst Bb.(all & st.occupation) st.side in
-      st.attackers <- Bb.((st.attackers + sliding) & st.occupation);
+      let sliders = sliders pos dst Bb.(all & st.occupation) st.side in
+      st.attackers <- Bb.((st.attackers + sliders) & st.occupation);
       (* Other side to move. *)
       st.side <- Piece.Color.opposite st.side;
       loop ()
